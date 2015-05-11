b1157f13d0ac77214112633fdbe818dea96013af
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-05-12 05:40:01 +0900

    update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 MSG=`cd contact_behavior_generation &&  git log . | head -6`;
-cd - && git commit contact_behavior_generation -m $MSG;
+echo $MSG;
+git commit contact_behavior_generation -m "update contact_behavior_generation: $MSG";
 
 MSG=`cd eus_gui &&  git log . | head -6`;
-cd - && git eus_gui -m $MSG;
+echo $MSG;
+git commit eus_gui -m "update eus_gui: $MSG";
 
 git push origin master;
