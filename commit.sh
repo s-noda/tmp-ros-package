321b3b8cf173a880fd427a49bb6cd77e231675a5
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-29 02:04:54 +0900

    update shell files update

--- commit.sh
+++ commit.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-MSG=`cd contact_behavior_generation &&  git log . | head -6`;
-echo $MSG;
-git commit contact_behavior_generation -m "update contact_behavior_generation: $MSG";
+for d in `ls`;
+do
+    if [ -d $d ];
+    then
+	echo "-- enter into $d";
+	cd $d;
+	MSG=`git log . | head -6`;
+	echo $MSG;
+	git commit . -m "update $d: $MSG";
+	git pull origin master;
+	cd ..;
+    fi
+done
 
-MSG=`cd eus_gui &&  git log . | head -6`;
-echo $MSG;
-git commit eus_gui -m "update eus_gui: $MSG";
 
-git push origin master;
+# MSG=`cd contact_behavior_generation &&  git log . | head -6`;
+# echo $MSG;
+# git commit contact_behavior_generation -m "update contact_behavior_generation: $MSG";
+
+# MSG=`cd eus_gui &&  git log . | head -6`;
+# echo $MSG;
+# git commit eus_gui -m "update eus_gui: $MSG";
+
+# git push origin master;
--- update.sh
+++ update.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
 git pull origin master;
-cd contact_behavior_generation;
-git pull origin master;
-cd ../eus_gui;
-git pull origin master ;
-cd ..;
+
+for d in `ls`;
+do
+    if [ -d $d ];
+    then
+	echo "-- enter into $d";
+	cd $d;
+	git pull origin master;
+	cd ..;
+    fi
+done
+
+# git pull origin master;
+# cd contact_behavior_generation;
+# git pull origin master;
+# cd ../eus_gui;
+# git pull origin master ;
+# cd ..;
